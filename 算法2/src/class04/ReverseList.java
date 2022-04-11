a9afea3eb734146d8092d9b9a280a6a69f1f328d
Doromv <DoromvQAQ@163.com>
2022-04-11 17:52:37 +0800

    2022-04-11左程云算法课新手班class5

---
+++ 算法2/src/class04/ReverseList.java
@@ -0,0 +1,48 @@
+package class04;
+
+/**
+ * @author Doromv
+ * @create 2022-04-11-15:58
+ */
+public class ReverseList {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            value = data;
+        }
+    }
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int data) {
+            value = data;
+        }
+    }
+    public static Node reverseLinkedList(Node head) {
+        Node pre=null;
+        Node next=null;
+        while (head.next!=null){
+            next=head.next;
+            head.next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+    public static DoubleNode reverseDoubleList(DoubleNode head) {
+        DoubleNode pre=null;
+        DoubleNode next=null;
+        while (head.next!=null){
+            next=head.next;
+            head.next=pre;
+            head.last=next;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+}
